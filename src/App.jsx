64e504e6f6d507e885622c278844c70b8e9009d8
Zanananane <jeannelebau@gmail.com>
2025-03-12 14:35:56 +0100

    base de donnee dexie

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import Dexie from "dexie";
+const db = new Dexie('Tablerecette');
+db.version(1).stores({recettes:"++int, titre, ingredients, temps, methode"})
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [recettes, setrecettes] = useState([]);
+  const [form, setForm] = useState({ titre: "", ingredients: "", temps: "", methode: "" });
 
+  // Charger les recettes
+  useEffect(() => {
+    chargerRecettes();
+  }, []);
+
+
+  const chargerRecettes = () => {
+    db.recettes.toArray().then(liste=>setrecettes(liste))
+      .catch((error)=>{console.log("erreur de chargement",error)});
+    console.log(recettes);
+    
+  };
+
+
+  // Gestion des changements dans le formulaire
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  // Ajouter un recette
+  const ajouterRecette = (e) => {
+    e.preventDefault();
+    db.recettes.add({titre:form.titre, ingredients:form.ingredients, temps:form.temps, methode:form.methode})
+    .then(()=>{console.log('Recette ajouté');})
+    .catch((error)=>{console.error("Erreur lors de l'ajout d'une recette",error);});
+    e.target.reset();
+  };
+
+  // Supprimer un recette
+  const supprimerRecette = (id) => {
+    db.recettes.delete(id).then(()=> console.log("Recette supprimée"))
+  };
+
+  // Mettre à jour un recette
+  const mettreAJourRecette = (e,id) => {
+    const recette = db.recettes.get(id);
+    if (form.titre != recette.titre || form.ingredients != recette.ingredients || form.temps != recette.temps || form.methode != recette.methode ) {
+      db.recettes.update(id,{titre: form.titre , ingredients: form.ingredients, temps: form.temps, methode: form.methode}).then((updated)=>{
+        if(updated){console.log("recette mise à jour");}
+        else{console.log("recette non trouvée");
+        }
+      })
+    }
+    chargerRecettes();
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
+
